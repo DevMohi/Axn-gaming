b24a4a0198ce6288891c4513df54856d01c1ff86
DevMohi <morphex72@gmail.com>
2022-04-05 04:08:58 +0600

    custom hooks added also data inititated

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useReviews from '../../hooks/useReviews';
 import axn from '../images/axn.png'
 
 const Home = () => {
+    const [reviews, setReviews] = useReviews()
+
+
     return (
-        <div className='container d-flex align-items-center '>
-            <div>
-                <h1 className='h1'>The Next Big Gaming Store</h1>
-                <h1 className='h2 text-success'>Looking For the Best in the town?</h1>
-                <p >Ever Wondered, where to buy them psn stocks and xbox stocks, guess what we got the solution for you, within a minute your codes will be delivered and you will be able to enjoy your game without any hesitation</p>
-                <button className='btn-dark btn'>Buy Now</button>
+        <div>
+            <div className='container d-flex align-items-center '>
+                <div>
+                    <h1 className='h1'>The Next Big Gaming Store</h1>
+                    <h1 className='h2 text-success'>Looking For the Best in the town?</h1>
+                    <p >Ever Wondered, where to buy them psn stocks and xbox stocks, guess what we got the solution for you, within a minute your codes will be delivered and you will be able to enjoy your game without any hesitation</p>
+                    <button className='btn-dark btn'>Buy Now</button>
+                </div>
+
+                <img src={axn} alt="pic not found" />
             </div>
 
-            <img src={axn} alt="pic not found" />
+            <div className='container'>
+                <h1>Reviews : {reviews.length}</h1>  
+            </div>
         </div>
     );
 };
