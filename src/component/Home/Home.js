74ba7e2da521a964f26de7e11b37cb1ab42b9f88
DevMohi <morphex72@gmail.com>
2022-04-05 04:47:05 +0600

    home page created , 3 reviews added according to the requirements

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import useReviews from '../../hooks/useReviews';
 import axn from '../images/axn.png'
+import './Home.css'
 
 const Home = () => {
     const [reviews, setReviews] = useReviews()
@@ -19,8 +21,25 @@ const Home = () => {
                 <img src={axn} alt="pic not found" />
             </div>
 
-            <div className='container'>
-                <h1>Reviews : {reviews.length}</h1>  
+            <div className='container main-container'>
+
+                {
+                    reviews.slice(0, 3).map(review => <div className='border rounded shadow-lg p-3 mb-5 bg-white rounded'>
+
+                        <div className='d-flex align-items-center justify-content-center rounded'>
+                            <img src={review.img} alt="" />
+                        </div>
+                        <div className='px-2 text-center'>
+                            <h3 className='pt-3'>Name : {review.name}</h3>
+                            <p className='h6 '>Review : {review.review}</p>
+                            <small className=''> Rating : {review.rating}</small>
+                        </div>
+
+                    </div>)
+                }
+            </div>
+            <div className='d-flex justify-content-center'>
+                <Link to='/reviews'><button className='btn btn-dark '>See More</button></Link>
             </div>
         </div>
     );
